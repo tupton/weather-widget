75ff41469337526adc868510a6a3d3fe060669c8
Thomas Upton <thomasupton@gmail.com>
2014-08-24 18:06:59 -0500

    Make location auto-detection configurable.
    This is a rather large change in that it removes all arguments to
    `fetch-forecast.py` from `index.coffee`. Config items are things like
    API keys and binary locations. Options and arguments to the script are
    things like location auto-detection, reverse geocoding, and providing a
    latitude and longitude or a location. These arguments are located in the
    fetch-forecast.args file, one per line.

--- weather.widget/fetch-forecast.py
+++ weather.widget/fetch-forecast.py
@@ -12,8 +12,12 @@ import argparse
 import ConfigParser
 import requests
 import json
+import subprocess
 
 class GeocodeError(Exception):
+    pass
+
+class LocationError(Exception):
     pass
 
 def _make_geocode_request(params, config):
@@ -56,7 +60,7 @@ def reverse_geocode(latitude, longitude, config):
     trimmed = ','.join(formatted.split(',')[:-2])
     return trimmed
 
-def fetch_forecast(latitude, longitude, config, location=None, reverse_geocode_location=False):
+def fetch_forecast(latitude, longitude, config, reverse_geocode_location=False):
     forecast_key = config.get('forecastio', 'api_key')
     forecast_url = "https://api.forecast.io/forecast/%(api_key)s/%(latitude)s,%(longitude)s" % {'api_key': forecast_key, 'latitude': latitude, 'longitude': longitude}
     forecast_params = {'units': 'auto', 'exclude': 'minutely,hourly,alerts,flags'}
@@ -64,26 +68,58 @@ def fetch_forecast(latitude, longitude, config, location=None, reverse_geocode_l
     req = requests.get(forecast_url, params=forecast_params)
     forecast = req.json()
 
-    if reverse_geocode_location is True:
-        if location is None:
-            try:
-                location = reverse_geocode(latitude, longitude, config)
-            except GeocodeError:
-                pass # Ignore and used whatever location was passed
-        else:
-            print('Since a location was provided, the option to reverse geocode the location is ignored', file=sys.stderr)
+    location = ''
+    if reverse_geocode_location:
+        try:
+            location = reverse_geocode(latitude, longitude, config)
+        except GeocodeError as e:
+            print(e, file=sys.stderr)
 
     forecast['formatted_location'] = location
 
     return forecast
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=__doc__)
+def detect_location(args, config):
+    if not args.auto_detect_location:
+        raise LocationError('The option to auto-detect a location was not provided.')
 
+    whereami = config.get('autodetect', 'whereami')
+    proc = subprocess.Popen(whereami, stdout=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if proc.returncode == 0:
+        # Successful output looks like:
+        #
+        #    Latitude: 30.264673
+        #    Longitude: -97.733859
+        #    Accuracy (m): 65.000000
+        #    Timestamp: 2014-08-24, 17:25:25 CDT
+        return tuple(l[1].strip() for l in [tuple(line.split(':', 1)) for line in stdout.split('\n')][:2])
+
+    raise LocationError('Detecting a location failed: %s' % (stdout,)) # whereami still prints to stdout if there's an error
+
+def get_location_from_args(args, config):
+    if args.latlon:
+        try:
+            lat, lon = (l.strip() for l in args.latlon.split(','))
+        except ValueError as e:
+            raise LocationError('Invalid latitude and longitude coordinates: %s\n%s' % (args.latlon, e))
+
+    elif args.location:
+        try:
+            lat, lon = geocode(args.location, config)
+        except GeocodeError as e:
+            raise LocationError('Invalid location: %s\n%s' % (args.location, e))
+
+    return (lat, lon)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__, fromfile_prefix_chars='@')
+
+    parser.add_argument('--auto-detect-location', action='store_true', help="If provided, attempt to detect the location with the `whereami` script, the location of which is provided in a config file.")
     parser.add_argument('--reverse-geocode', action='store_true', help="If provided, reverse geocode the given location and include it in the JSON forecast. If you provide a human-readable location, this option is ignored and that location is used in the output.")
     parser.add_argument('--config-file', '-c', default='weather.conf', help="The file to use for config options, including the forecast.io and Google geocode API keys.")
 
-    location_group = parser.add_mutually_exclusive_group(required=True)
+    location_group = parser.add_mutually_exclusive_group()
     location_group.add_argument('--latlon', help="The comma-separated latitude and longitude of the location to fetch forecast for")
     location_group.add_argument('--location', help="A human-readable location that will be geocoded and used as the location to fetch forecast for")
 
@@ -92,21 +128,15 @@ if __name__ == "__main__":
     config = ConfigParser.SafeConfigParser()
     config.read(args.config_file)
 
-    if args.latlon:
+    try:
         try:
-            lat, lon = (l.strip() for l in args.latlon.split(','))
-        except ValueError as e:
-            print('Invalid latitude and longitude coordinates: %s' % (args.latlon,), file=sys.stderr)
-            print('Error: %s' % (e,), file=sys.stderr)
-            sys.exit(1)
+            lat, lon = detect_location(args, config)
+        except Exception as e:
+            print('Unable to detect location: %s' % (e,), file=sys.stderr)
+            lat, lon = get_location_from_args(args, config)
+    except LocationError as e:
+        print('Unable to determine a location to request forecast for: %s', e)
+        sys.exit(1)
 
-    elif args.location:
-        try:
-            lat, lon = geocode(args.location, config)
-        except GeocodeError as e:
-            print('Invalid location: %s' % (args.location,), file=sys.stderr)
-            print('Error: %s' % (e,), file=sys.stderr)
-            sys.exit(1)
-
-    response = fetch_forecast(lat, lon, config, location=args.location, reverse_geocode_location=args.reverse_geocode)
+    response = fetch_forecast(lat, lon, config, reverse_geocode_location=args.reverse_geocode)
     print(json.dumps(response))
